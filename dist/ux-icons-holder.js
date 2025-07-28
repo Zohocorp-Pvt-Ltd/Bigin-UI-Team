eed3155423aba99cc94c8b000d47a44d75d0e9eb
Hithesh <hithesh.m@zohocorp.com>
2025-07-28 16:50:50 +0530

    Updated changes

--- dist/ux-icons-holder.js
+++ dist/ux-icons-holder.js
@@ -1,5 +1,5 @@
 const uxIconsHTML = `
-<div style="display: none;">
+<div style="visibility: hidden;height:0;width:0;pointer-events: none;">
         <svg xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <style>
